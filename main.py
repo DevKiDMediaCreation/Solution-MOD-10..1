3304102af799f8a5994cc1f6ccc19fd9a35cb823
DevKiD <75792299+DevKiDMediaCreation@users.noreply.github.com>
2024-04-29 15:44:26 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+
+r = []
+
+def mod(x, m , n):
+  return x % m == n
+
+for i in range(10):
+  for j in range(100000):
+    if mod(j, 10 - i, 9 - i):
+      if i == 0:
+        if j not in r:
+          r.append(j)
+    else:
+      if j in r:
+        # Remove the element from the list with value of j
+        r.pop(r.index(j))
+print(r)
